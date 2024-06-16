0ad78e465fb9e9a0b4bd3f69d3377265f8d44668
harsha200210 <harshadilanbb2020@gmail.com>
2024-06-16 15:39:53 +0530

    generate report

--- src/main/java/com/zer0bugs/controller/ReportCustomViewController.java
+++ src/main/java/com/zer0bugs/controller/ReportCustomViewController.java
@@ -1,18 +1,29 @@
 package com.zer0bugs.controller;
 
+import com.itextpdf.kernel.pdf.PdfDocument;
+import com.itextpdf.kernel.pdf.PdfWriter;
+import com.itextpdf.layout.Document;
+import com.itextpdf.layout.element.Paragraph;
+import com.itextpdf.layout.element.Table;
+import com.itextpdf.layout.property.TextAlignment;
+import com.itextpdf.layout.property.UnitValue;
 import com.zer0bugs.model.tm.MonthViewTm;
 import com.zer0bugs.repo.CustomViewRepo;
-import com.zer0bugs.repo.MonthViewRepo;
 import com.zer0bugs.util.GlobalVariables;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.*;
 import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.input.MouseEvent;
+import javafx.stage.FileChooser;
+import javafx.stage.Stage;
 import lombok.Setter;
 
+import java.io.File;
 import java.sql.Date;
 import java.sql.SQLException;
 
@@ -123,7 +134,74 @@ public class ReportCustomViewController {
 
     @FXML
     void genaratePdfBtnOnAction(ActionEvent event) {
-
+        FileChooser fileChooser = new FileChooser();
+        fileChooser.setTitle("Save PDF");
+        fileChooser.getExtensionFilters().add(new FileChooser.ExtensionFilter("PDF Files", "*.pdf"));
+        Stage primaryStage = new Stage();
+        File file = fileChooser.showSaveDialog(primaryStage);
+        if (file != null) {
+            if (!file.getPath().endsWith(".pdf")) {
+                file = new File(file.getPath() + ".pdf");
+            }
+            System.out.println("Saving to: " + file.getAbsolutePath());  // Debugging line
+            try {
+                createPDF(file.getAbsolutePath(), monthViewTabel);
+                new Alert(Alert.AlertType.CONFIRMATION,"PDF saved successfully").show();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        } else {
+            System.out.println("Save operation cancelled.");  // Debugging line
+        }
     }
 
+    private void createPDF(String dest, TableView<MonthViewTm> tableView) throws Exception {
+        PdfWriter writer = new PdfWriter(dest);
+        PdfDocument pdfDoc = new PdfDocument(writer);
+        Document document = new Document(pdfDoc);
+
+        String title = "Report";
+
+        Paragraph pdfTitle = new Paragraph(title)
+                .setFontSize(18)
+                .setBold()
+                .setTextAlignment(TextAlignment.CENTER); // Center align the title
+        document.add(pdfTitle);
+
+        // Add some space between the title and the table
+        document.add(new Paragraph("\n"));
+
+        String subDescription = "From : " + startDate + "\t\t\t\t To : " + endDate ;
+
+        Paragraph pdfSubDescription = new Paragraph(subDescription)
+                .setFontSize(12)
+                .setItalic();
+        document.add(pdfSubDescription);
+
+        // Add some space between the sub-description and the table
+        document.add(new Paragraph("\n"));
+
+        float availableWidth = document.getPdfDocument().getDefaultPageSize().getWidth()
+                - document.getLeftMargin() - document.getRightMargin();
+
+        // Create a table with the same number of columns as the TableView
+        Table pdfTable = new Table(UnitValue.createPercentArray(new float[]{1, 1, 1}))
+                .useAllAvailableWidth();
+
+        // Add table headers
+        for (TableColumn<MonthViewTm, ?> column : tableView.getColumns()) {
+            pdfTable.addHeaderCell(new Paragraph(column.getText()));
+        }
+
+        // Add table rows
+        ObservableList<MonthViewTm> data = tableView.getItems();
+        for (MonthViewTm viewTm : data) {
+            pdfTable.addCell(new Paragraph(String.valueOf(viewTm.getData())));
+            pdfTable.addCell(new Paragraph(String.valueOf(viewTm.getTotalOrder())));
+            pdfTable.addCell(new Paragraph(String.valueOf(viewTm.getTotalTaken())));
+        }
+
+        document.add(pdfTable);
+        document.close();
+    }
 }
--- src/main/java/com/zer0bugs/controller/ReportDayViewController.java
+++ src/main/java/com/zer0bugs/controller/ReportDayViewController.java
@@ -1,7 +1,15 @@
 package com.zer0bugs.controller;
 
+import com.itextpdf.kernel.pdf.PdfDocument;
+import com.itextpdf.kernel.pdf.PdfWriter;
+import com.itextpdf.layout.Document;
+import com.itextpdf.layout.element.Paragraph;
+import com.itextpdf.layout.element.Table;
+import com.itextpdf.layout.property.TextAlignment;
+import com.itextpdf.layout.property.UnitValue;
 import com.jfoenix.controls.JFXButton;
 import com.zer0bugs.model.TimePicker;
+import com.zer0bugs.model.tm.MonthViewTm;
 import com.zer0bugs.model.tm.OrderTm;
 import com.zer0bugs.repo.DayViewRepo;
 import com.zer0bugs.util.GlobalVariables;
@@ -15,10 +23,14 @@ import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.input.MouseEvent;
+import javafx.stage.FileChooser;
+import javafx.stage.Stage;
 import lombok.Setter;
 
+import java.io.File;
 import java.sql.Date;
 import java.sql.SQLException;
+import java.time.LocalDate;
 import java.time.LocalTime;
 
 public class ReportDayViewController {
@@ -149,5 +161,89 @@ public class ReportDayViewController {
     }
 
     public void genaratePdfBtnOnAction(ActionEvent event) {
+        FileChooser fileChooser = new FileChooser();
+        fileChooser.setTitle("Save PDF");
+        fileChooser.getExtensionFilters().add(new FileChooser.ExtensionFilter("PDF Files", "*.pdf"));
+        Stage primaryStage = new Stage();
+        File file = fileChooser.showSaveDialog(primaryStage);
+        if (file != null) {
+            if (!file.getPath().endsWith(".pdf")) {
+                file = new File(file.getPath() + ".pdf");
+            }
+            //System.out.println("Saving to: " + file.getAbsolutePath());  // Debugging line
+            try {
+                createPDF(file.getAbsolutePath(), tblOrder);
+                new Alert(Alert.AlertType.CONFIRMATION,"PDF saved successfully").show();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        } else {
+            System.out.println("Save operation cancelled.");  // Debugging line
+        }
+    }
+
+    private void createPDF(String dest, TableView<OrderTm> tableView) throws Exception {
+        PdfWriter writer = new PdfWriter(dest);
+        PdfDocument pdfDoc = new PdfDocument(writer);
+        Document document = new Document(pdfDoc);
+
+        String title = "Report";
+
+        Paragraph pdfTitle = new Paragraph(title)
+                .setFontSize(18)
+                .setBold()
+                .setTextAlignment(TextAlignment.CENTER); // Center align the title
+        document.add(pdfTitle);
+
+        // Add some space between the title and the table
+        document.add(new Paragraph("\n"));
+
+        String subDescription = "";
+        if (startTime != null && endTime != null) {
+            subDescription = "Date : " + LocalDate.now() + "\t\t\t From : " + startTime + "\t To : " + endTime;
+        } else if (endTime == null) {
+            subDescription = "Date : " + LocalDate.now() + "\t\t\t From : " + startTime;
+        } else if (startTime == null){
+            subDescription = "Date : " + LocalDate.now();
+        }
+
+        Paragraph pdfSubDescription = new Paragraph(subDescription)
+                .setFontSize(12)
+                .setItalic();
+        document.add(pdfSubDescription);
+
+        // Add some space between the sub-description and the table
+        document.add(new Paragraph("\n"));
+
+        float availableWidth = document.getPdfDocument().getDefaultPageSize().getWidth()
+                - document.getLeftMargin() - document.getRightMargin();
+
+        // Create a table with the same number of columns as the TableView
+        Table pdfTable = new Table(UnitValue.createPercentArray(new float[]{1, 1, 1, 1, 1, 1}))
+                .useAllAvailableWidth();
+
+        // Add table headers
+        for (TableColumn<OrderTm, ?> column : tableView.getColumns()) {
+            pdfTable.addHeaderCell(new Paragraph(column.getText()));
+        }
+
+        // Add table rows
+        ObservableList<OrderTm> data = tableView.getItems();
+        for (OrderTm viewTm : data) {
+            pdfTable.addCell(new Paragraph(String.valueOf(viewTm.getName())));
+            pdfTable.addCell(new Paragraph(String.valueOf(viewTm.getDepartment())));
+            pdfTable.addCell(new Paragraph(String.valueOf(viewTm.getDate())));
+            pdfTable.addCell(new Paragraph(String.valueOf(viewTm.getPlaceTime())));
+            pdfTable.addCell(new Paragraph(String.valueOf(viewTm.getTaken())));
+            if (viewTm.getTakenTime() != null) {
+                pdfTable.addCell(new Paragraph(String.valueOf(viewTm.getTakenTime())));
+            } else {
+                pdfTable.addCell(new Paragraph(""));
+            }
+
+        }
+
+        document.add(pdfTable);
+        document.close();
     }
 }
