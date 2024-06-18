d547dfeb6fa3510dd5ef352663111834803bc790
Shashi Madushan <156106584+shashimadu@users.noreply.github.com>
2024-06-18 10:49:13 +0530

    settings ui changed

--- src/main/java/com/zer0bugs/controller/SettingsViewController.java
+++ src/main/java/com/zer0bugs/controller/SettingsViewController.java
@@ -1,5 +1,30 @@
 package com.zer0bugs.controller;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
 public class SettingsViewController {
+
+    @FXML
+    private TextField currentPwTextFiled;
+
+    @FXML
+    private Label currentUnameLbel;
+
+    @FXML
+    private TextField newPwTextField;
+
+    @FXML
+    private TextField newPwrReenterTextField;
+
+    @FXML
+    private TextField newUnameTExtField;
+
+    @FXML
+    void changeBtnOnAction(ActionEvent event) {
+
+    }
 
 }
